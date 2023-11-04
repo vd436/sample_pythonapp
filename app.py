ce5c7000cd0d5fb59c965d2f7f1f6659ec716f79
vd436 <108041538+vd436@users.noreply.github.com>
2023-11-04 08:05:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-# Import the Flask module
 from flask import Flask
 
-# Create a Flask web application
 app = Flask(__name__)
 
-# Define a route and its handler function
 @app.route('/')
 def hello_world():
     return 'Welcome.. I am a sample app!'
 
-# Run the application
 if __name__ == '__main__':
     app.run()
