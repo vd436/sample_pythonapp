ff8159ddbf36b7da40065327745e302b487d7db9
vd436 <108041538+vd436@users.noreply.github.com>
2023-11-04 08:48:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome.. I am a sample app!'
+    return 'Welcome.. I am a sample app!!'
 
 if __name__ == '__main__':
     app.run()
